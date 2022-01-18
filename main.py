37c0ea2d23d235849f5a614cca01595a16d134ac
Adnan Pasha <92546855+adnanpasha7@users.noreply.github.com>
2022-01-18 09:50:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 STOCK_API_KEY = "HX103TTTEQBD4EQP"
 NEWS_API_KEY = "3e2173cade4b46dfa49f9c6f8eed34a6"
 TWILIO_SID = "YOUR_SID"
-AUTH_TOKEN = "1eea9f1f6abd2bea7300ea1d1d3af658"
+AUTH_TOKEN = "YOUR_AUTH_TOKEN_FROM_TWILIO"
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
 stock_param = {
     "function" : "TIME_SERIES_DAILY",
@@ -32,7 +31,7 @@ day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 print(day_before_yesterday_closing_price)
 
-#Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+#Find the difference between 1 and 2. 
 difference = float(yesterdays_closing_price) - float(day_before_yesterday_closing_price)
 print(difference)
 up_down = None
@@ -69,19 +68,8 @@ if diff_percent > 1:
     for article in formatted_article:
         message = client.messages.create(
             body=article,
-            from_="+19378844366",
-            to="+919108811142",
+            from_="YOUR_TWILIO'S_PHONE_NUMBER",
+            to="YOUR_PHONR_NUMBER",
         )
 
-
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
